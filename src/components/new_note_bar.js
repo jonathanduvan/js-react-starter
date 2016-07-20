a9e14d144b832ae023ab437a2c86ce809ba2e1ae
jonathanduvan <jonathan.d.gonzalez.18@dartmouth.edu>
2016-07-20 17:06:25 -0400

    name edits

--- src/components/new_note_bar.js
+++ src/components/new_note_bar.js
@@ -1,11 +1,13 @@
 
 import React, { Component } from 'react';
 
-class NewNodeBar extends Component {
+class NewNoteBar extends Component {
   constructor(props) {
     super(props);
+
     this.state = { title: '' }; // title of the new node
     this.onInputChange = this.onInputChange.bind(this);
+    this.addNote = this.addNote.bind(this);
   }
 
   // What happens when the user inputs a title into the new node bar
@@ -14,17 +16,19 @@ class NewNodeBar extends Component {
     this.setState({ title: event.target.value });
   }
 
+  // function to add new node
   addNote(event) {
-
+    this.props.create(this.state.title);
   }
 
   render() {
     return (
-      <div id="newnodebar">
+      <div id="newnotebar">
         <input onChange={this.onInputChange} value={this.state.title} placeholder="new note title" />
+        <button onClick={this.onCreate}>Create</button>
       </div>
     );
   }
 }
 
-export default NewNodeBar;
+export default NewNoteBar;
